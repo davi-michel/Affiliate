5b231e3c1c03dd1343b60ac1867ed120a1f31adb
Davi Silva | Opus CM <davisilva@opuscm.com.br>
2025-10-16 17:52:41 -0300

    Alteração do BackGround Geral

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header.jsx";
 
 function App() {
   return (
-    <div className="font-poppins">
+    <div className="font-poppins bg-[#F6EDE1] min-h-screen">
       <Header/>
     </div>
   );
