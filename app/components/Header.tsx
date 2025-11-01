c6386a5d4e6cd89765b1afd98a46cdb040ecf897
Davi <davimichelcomsilva@gmail.com>
2025-11-01 14:44:34 -0300

    Projeto migrado para Next

---
+++ app/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <div className="flex justify-center gap-32">
+            <h1 className="text-4xl">
+                PromoLink
+            </h1>
+            <input type="text" placeholder="Buscar Promoções..." className="rounded-lg p-1 px-3 w-1/3 bg-[#EFEAE4] placeholder-[#8B8B8B]" />
+            <div className="flex text-xl gap-7 my-auto font-normal text-[#2e2e2e]">
+                <h2>Home</h2>
+                <h2>Categorias</h2>
+                <h2>Sobre nós</h2>
+            </div>
+        </div>
+    )
+}
---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+export default function HeroSection(){
+    return(
+        <section className="flex bg-[#EFEAE4]">
+            <div className="border border-gray-400 px-16 py-2 rounded-xl">
+                
+            </div>
+            <div>
+                <h2 className="font-sans text-3xl">
+                    PROMOÇÕES INCRÍVEIS!
+                </h2>
+                <h2 className="font-noto text-xl">
+                    As melhores ofertas em um só lugar
+                </h2>
+            </div>
+        </section>
+    )
+}
