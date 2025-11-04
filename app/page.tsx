bc269848629be721e5666582b5fa5f94491f4511
whitwander <vcmikhaelcp@gmail.com>
2025-11-03 21:13:46 -0300

    footer and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/app/components/Header";
 import HeroSection from "@/app/components/HeroSection";
 import CardView from "./components/CardView";
+import { Instagram, Twitter, Youtube } from 'lucide-react';
 
 function App() {
   return (
@@ -9,6 +10,18 @@ function App() {
         <Header />
         <div className="px-20">
           <HeroSection />
+        </div>
+        <div className="border-t mt-10 border-t-gray-300 font-normal text-md mx-auto px-20 w-5/6 py-5 flex justify-between items-center">
+          <div className="flex gap-8">
+            <a href="#">Política de Privacidade</a>
+            <a href="#">Contato</a>
+            <a href="#">Sobre nós</a>
+          </div>
+          <div className="flex gap-4">
+            <Twitter size={24} className="cursor-pointer" />
+            <Instagram size={24} className="cursor-pointer" />
+            <Youtube size={24} className="cursor-pointer" />
+          </div>
         </div>
       </div>
       <div className="max-w-[30%] mx-auto">
